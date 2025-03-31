8bef1a8ddc788651d2b982b8cfef07fc8104f752
mrpark219 <mrpark219@gmail.com>
2025-03-31 20:52:51 +0900

    ✨ catalog-service /catalogs 요청 추가 및 api-gateway-service 연동
    - catalog-service /health-check, /catalogs 요청 추가
    - catalog-service 실행 시 초기 데이터 추가하도록 설정
    - api-gateway-service에 catalog-service 등록

---
+++ catalog-service/src/main/java/com/example/catalogservice/service/CatalogService.java
@@ -0,0 +1,10 @@
+package com.example.catalogservice.service;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+
+public interface CatalogService {
+
+	Iterable<CatalogEntity> getAllCatalogs();
+
+
+}
