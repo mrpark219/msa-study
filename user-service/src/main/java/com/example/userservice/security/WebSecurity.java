d3224c8b026d7ef3fc606ee59da2c22bf6f86b6d
mrpark219 <mrpark219@gmail.com>
2025-03-31 19:38:11 +0900

    ✨ user-service security 연동
    - BCryptPasswordEncoder 빈 등록 추가
    - 회원가입 시 비밀번호 암호화 추가
    - Security 설정 추가

---
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,32 @@
+package com.example.userservice.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurity {
+
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.headers((headers) ->
+				headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+			)
+			.authorizeHttpRequests(authorizationHttpRequests -> authorizationHttpRequests
+				.requestMatchers("/users/**").permitAll()
+				.requestMatchers(PathRequest.toH2Console()).permitAll()
+			);
+
+		return http.build();
+	}
+
+}
--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,9 +14,11 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
 
-	public UserServiceImpl(UserRepository userRepository) {
+	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
@@ -27,7 +30,7 @@ public class UserServiceImpl implements UserService {
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = mapper.map(userDto, UserEntity.class);
 
-		userEntity.setEncryptedPwd("encrypted_password");
+		userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 
 		userRepository.save(userEntity);
 
