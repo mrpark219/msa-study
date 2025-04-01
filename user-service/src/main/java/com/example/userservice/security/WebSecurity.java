f22061e8ef42d9ee7ba51a89990748f760a2c6ae
mrpark219 <mrpark219@gmail.com>
2025-04-01 13:00:40 +0900

    ✨ user-service AuthenticationFilter 추가 및 loadUserByUsername() 구현 추가

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,20 +1,45 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurity {
 
+	private final UserService userService;
+
+	private final BCryptPasswordEncoder passwordEncoder;
+
+	private final Environment env;
+
+	public WebSecurity(UserService userService, BCryptPasswordEncoder passwordEncoder, Environment env) {
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
+		this.env = env;
+	}
+
 	@Bean
-	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+		builder.userDetailsService(userService).passwordEncoder(passwordEncoder);
+		return builder.build();
+	}
+
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
 
 		http
 			.csrf(AbstractHttpConfigurer::disable)
@@ -22,12 +47,20 @@ public class WebSecurity {
 				headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
 			)
 			.authorizeHttpRequests(authorizationHttpRequests -> authorizationHttpRequests
-				.requestMatchers("/users/**").permitAll()
-				.requestMatchers("/**").permitAll()
+				.requestMatchers("/**").access(
+					new WebExpressionAuthorizationManager(
+						"hasIpAddress('192.168.0.3')" // 로컬 컴퓨터 IP
+					)
+				)
 				.requestMatchers(PathRequest.toH2Console()).permitAll()
-			);
+			)
+			.addFilter(getAuthenticationFilter(authenticationManager));
 
 		return http.build();
+	}
+
+	private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) {
+		return new AuthenticationFilter(authenticationManager);
 	}
 
 }
---
+++ user-service/src/main/java/com/example/userservice/vo/RequestLogin.java
@@ -0,0 +1,21 @@
+package com.example.userservice.vo;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestLogin {
+
+	@NotNull(message = "Email cannot be null")
+	@Size(min = 2, message = "Email not be less than two characters")
+	@Email
+	private String email;
+
+	@NotNull(message = "Password cannot be null")
+	@Size(min = 8, message = "Password must not be greater than 8 characters")
+	private String password;
+}
