0d1e815cf5568a796f2364b37d67277e1db2dce3
mrpark219 <mrpark219@gmail.com>
2025-04-11 22:12:10 +0900

    ✨ user-service FeignClient 관련 오류 처리를 try-catch에서 FeignErrorDecoder로 변경

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
-import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -68,14 +67,7 @@ public class UserServiceImpl implements UserService {
 
 		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-		List<ResponseOrder> orderList = null;
-
-		try {
-			orderList = orderServiceClient.getOrders(userId);
-		}
-		catch(FeignException ex) {
-			log.error(ex.getMessage());
-		}
+		List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
 
 		userDto.setOrders(orderList);
 
