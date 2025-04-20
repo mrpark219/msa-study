00db11bb3771b4558c285e69fdef5e0ae9f92c26
mrpark219 <mrpark219@gmail.com>
2025-04-20 12:37:21 +0900

    ✨ api-gateway-service, order-service, user-service Prometheus 관련 의존성 및 설정 추가
    - api-gateway-service, order-service, user-service pom.xml micrometer-registry-prometheus 의존성 추가
    - api-gateway-service, order-service, user-service actuator info, metrics, prometheus 설정 추가
    - user-service UserController @Timed 설정 추가
    - user-service MetricsConfig 설정 추가

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
@@ -67,6 +68,7 @@ public class UserController {
 	}
 
 	@GetMapping("/health-check")
+	@Timed(value = "users.status", longTask = true)
 	public String status() {
 		return String.format("It's Working in User Service"
 			+ ", port(local.server.port)=" + env.getProperty("local.server.port")
@@ -76,6 +78,7 @@ public class UserController {
 	}
 
 	@GetMapping("/welcome")
+	@Timed(value = "users.welcome", longTask = true)
 	public String welcome() {
 		//return env.getProperty("greeting.message");
 		return greeting.getMessage();
