03228aab5836c26bae7d3dce2e6d810432fe2619
mrpark219 <mrpark219@gmail.com>
2025-05-05 21:57:28 +0900

    ✨ order-service kafka 설정 정보를 application.yml에서 읽어오도록 변경

--- order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.example.orderservice.messagequeue;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,12 +17,15 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+	@Value("${kafka.producer.bootstrap-server}")
+	private String bootstrapServers;
+
 	@Bean
 	public ProducerFactory<String, String> producerFactory() {
 
 		Map<String, Object> properties = new HashMap<>();
 
-		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
